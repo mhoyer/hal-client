bb6a05da8df0c7a85034a94d254ab3298439ec54
Marcel Hoyer <mhoyer@pixelplastic.de>
2018-04-09 21:19:36 +0200

    add `./index.ts`

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { HalClient } from './lib/hal-client';
+import { HalLink } from './lib/hal-link';
+import { HalResource } from './lib/hal-resource';
+import { LazyResource } from './lib/lazy-resource';
+import { ResourceFetcher } from './lib/resource-fetcher';
+import { StaticResource } from './lib/static-resource';
+
+export {
+    HalClient,
+    HalLink,
+    HalResource,
+    LazyResource,
+    ResourceFetcher,
+    StaticResource,
+};
